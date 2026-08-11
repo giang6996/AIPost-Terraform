30ce04b13f60f76ecd2afd41d36882e6cd97357e
giang6996 <giangnguyencanh69@gmail.com>
2026-08-12 05:09:49 +0700

    feat: adding HTTP egress rule to Jenkins Security Group

--- infrastructures/modules/security/main.tf
+++ infrastructures/modules/security/main.tf
@@ -136,3 +136,13 @@ resource "aws_vpc_security_group_egress_rule" "jenkins_https" {
 
   cidr_ipv4 = "0.0.0.0/0"
 }
+
+resource "aws_vpc_security_group_egress_rule" "jenkins_http" {
+  security_group_id = aws_security_group.jenkins.id
+
+  ip_protocol = "tcp"
+  from_port   = 80
+  to_port     = 80
+
+  cidr_ipv4 = "0.0.0.0/0"
+}
