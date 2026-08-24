6aa6e91d2e1b1ba30201ffc5ec282b493a1d4bb2
giang6996 <giangnguyencanh69@gmail.com>
2026-08-25 04:27:23 +0700

    feat: configure Access Entries and aws-auth dual mode for EKS

--- infrastructures/modules/eks/main.tf
+++ infrastructures/modules/eks/main.tf
@@ -40,6 +40,11 @@ resource "aws_eks_cluster" "this" {
     endpoint_public_access  = true
   }
 
+  access_config {
+    authentication_mode                         = "API_AND_CONFIG_MAP" # EKS Access Entries and aws-auth ConfigMap
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   depends_on = [
     aws_iam_role_policy_attachment.eks_cluster_policy
   ]
